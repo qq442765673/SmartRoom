e4cffaea2eec33970ef897ded9a78ed2bad7ce5b
LINGGE Ma <lxm008@cs.bham.ac.uk>
2022-08-27 23:44:54 +0100

    app finnal

--- Smarthouse/app/src/main/java/com/example/smarthouse/MainActivity.java
+++ Smarthouse/app/src/main/java/com/example/smarthouse/MainActivity.java
@@ -8,9 +8,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.content.Intent;
+import android.widget.Button;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 
 import java.text.SimpleDateFormat;
@@ -26,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private String MotionId = "62cc43441d8472031ec566c1";
     private String LightswtichId = "62edabc81d84720d2adde8e9";
     private String fanswtichId = "62edac161d84720d2978be8c";
+    private String autofanId = "630a86cf373ded000cec2c0a";
+    private String autolightId = "630a8547ece0fd000c25d10d";
     private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
     public String temperature = "0";
     public String Humidity = "0";
@@ -33,131 +35,27 @@ public class MainActivity extends AppCompatActivity {
     public Float Motion = 0f;
     public Float Lightswtich = 0f;
     public Float fanswtich = 0f;
+    public Float autofan = 0f;
+    public Float autolight = 0f;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        (new UbidotsClient()).handleUbidots(tempVarId, API_KEY, result -> {
-            Log.d("Chart", "======== On data Ready ===========");
-            float ccc = result.get(0).value;
-            temperature = String.valueOf(ccc);
-        });
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                TextView tempureturetext = (TextView) findViewById(R.id.textView5);
-                Log.d("tempure", temperature);
-                tempureturetext.setText(temperature);
-            }
-        }, 2000);
+        Update();
 
-
-        (new UbidotsClient()).handleUbidots(humVarId, API_KEY, result -> {
-            Log.d("Chart", "======== On data Ready ===========");
-            float ccc = result.get(0).value;
-            Humidity = String.valueOf(ccc);
-        });
-        Handler handler1 = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                TextView Humiditytext = (TextView) findViewById(R.id.textView6);
-                Log.d("Humidity", Humidity);
-                Humiditytext.setText(Humidity);
+        Button button = (Button) findViewById(R.id.update);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Update();
             }
-        }, 2000);
-
-        (new UbidotsClient()).handleUbidots(LightId, API_KEY, result -> {
-            Log.d("Chart", "======== On data Ready ===========");
-            float ccc = result.get(0).value;
-            Lightsens = String.valueOf(ccc);
         });
-        Handler handler2 = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                TextView Lightsenstext = (TextView) findViewById(R.id.textView7);
-                Log.d("Lightsens", Lightsens);
-                Lightsenstext.setText(Lightsens);
-            }
-        }, 2000);
 
-        (new UbidotsClient()).handleUbidots(LightswtichId, API_KEY, result -> {
-            Log.d("Chart", "======== On data Ready ===========");
-            float ccc = result.get(0).value;
-            Lightswtich = ccc;
-        });
-        Handler handler3 = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                TextView Lightswtichtext = (TextView) findViewById(R.id.textView8);
-                Log.d("Lightswtich", Lightswtich.toString());
-                if(Lightswtich==1){
-                    Lightswtichtext.setText("on");
-                }
-                else if (Lightswtich==0){
-                    Lightswtichtext.setText("off");
-                }
-                else {
-                    Lightswtichtext.setText("error");
-                }
-            }
-        }, 2000);
-
-        (new UbidotsClient()).handleUbidots(fanswtichId, API_KEY, result -> {
-            Log.d("Chart", "======== On data Ready ===========");
-            float ccc = result.get(0).value;
-            fanswtich = ccc;
-        });
-        Handler handler4 = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                TextView fanswtichtext = (TextView) findViewById(R.id.textView9);
-                Log.d("fanswtich", fanswtich.toString());
-                if(fanswtich==1){
-                    fanswtichtext.setText("on");
-                }
-                else if (fanswtich==0){
-                    fanswtichtext.setText("off");
-                }
-                else {
-                    fanswtichtext.setText("error");
-                }
-            }
-        }, 2000);
-
-        (new UbidotsClient()).handleUbidots(MotionId, API_KEY, result -> {
-            Log.d("Chart", "======== On data Ready ===========");
-            float ccc = result.get(99).value;
-            Motion = ccc;
-        });
-        Handler handler5 = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                TextView Motiontext = (TextView) findViewById(R.id.textView11);
-                Log.d("Motion", Motion.toString());
-                if(Motion==1){
-                    Motiontext.setText("Occupied");
-                }
-                else if (Motion==0){
-                    Motiontext.setText("Empty");
-                }
-                else {
-                    Motiontext.setText("error");
-                }
-            }
-        }, 2000);
     }
 
-    public void onCreateView (View view){
-        // Inflate the layout for this fragment
-
+    public void Update()
+    {
         (new UbidotsClient()).handleUbidots(tempVarId, API_KEY, result -> {
-            Log.d("Chart", "======== On data Ready ===========");
+            Log.d("tempVarId", "======== On data Ready ===========");
             float ccc = result.get(0).value;
             temperature = String.valueOf(ccc);
         });
@@ -171,9 +69,8 @@ public class MainActivity extends AppCompatActivity {
             }
         }, 2000);
 
-
         (new UbidotsClient()).handleUbidots(humVarId, API_KEY, result -> {
-            Log.d("Chart", "======== On data Ready ===========");
+            Log.d("humVarId", "======== On data Ready ===========");
             float ccc = result.get(0).value;
             Humidity = String.valueOf(ccc);
         });
@@ -188,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
         }, 2000);
 
         (new UbidotsClient()).handleUbidots(LightId, API_KEY, result -> {
-            Log.d("Chart", "======== On data Ready ===========");
+            Log.d("LightId", "======== On data Ready ===========");
             float ccc = result.get(0).value;
             Lightsens = String.valueOf(ccc);
         });
@@ -203,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
         }, 2000);
 
         (new UbidotsClient()).handleUbidots(LightswtichId, API_KEY, result -> {
-            Log.d("Chart", "======== On data Ready ===========");
+            Log.d("LightswtichId", "======== On data Ready ===========");
             float ccc = result.get(0).value;
             Lightswtich = ccc;
         });
@@ -226,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
         }, 2000);
 
         (new UbidotsClient()).handleUbidots(fanswtichId, API_KEY, result -> {
-            Log.d("Chart", "======== On data Ready ===========");
+            Log.d("fanswtichId", "======== On data Ready ===========");
             float ccc = result.get(0).value;
             fanswtich = ccc;
         });
@@ -249,7 +146,7 @@ public class MainActivity extends AppCompatActivity {
         }, 2000);
 
         (new UbidotsClient()).handleUbidots(MotionId, API_KEY, result -> {
-            Log.d("Chart", "======== On data Ready ===========");
+            Log.d("MotionId", "======== On data Ready ===========");
             float ccc = result.get(0).value;
             Motion = ccc;
         });
@@ -271,6 +168,53 @@ public class MainActivity extends AppCompatActivity {
             }
         }, 2000);
 
+        (new UbidotsClient()).handleUbidots(autofanId, API_KEY, result -> {
+            Log.d("autofanId", "======== On data Ready ===========");
+            float ccc = result.get(0).value;
+            autofan = ccc;
+        });
+        Handler handler6 = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                TextView autofantext = (TextView) findViewById(R.id.textView19);
+                Log.d("autofan", autofan.toString());
+                if(autofan==1){
+                    autofantext.setText("on");
+                }
+                else if (autofan==0){
+                    autofantext.setText("off");
+                }
+                else {
+                    autofantext.setText("error");
+                }
+            }
+        }, 2000);
+
+        (new UbidotsClient()).handleUbidots(autolightId, API_KEY, result -> {
+            Log.d("autolightId", "======== On data Ready ===========");
+            float ccc = result.get(0).value;
+            autolight = ccc;
+        });
+        Handler handler7 = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                TextView autolighttext = (TextView) findViewById(R.id.textView18);
+                Log.d("autolightId", autolight.toString());
+                if(autolight==1){
+                    autolighttext.setText("on");
+                }
+                else if (autolight==0){
+                    autolighttext.setText("off");
+                }
+                else {
+                    autolighttext.setText("error");
+                }
+            }
+        }, 2000);
+
+        Toast.makeText(this,"Updating",Toast.LENGTH_SHORT).show();
     }
 
     public void lighswitch (View view)
@@ -284,7 +228,8 @@ public class MainActivity extends AppCompatActivity {
         else {
             sendrequest1.send(this,"lightswitch",1);}
 
-
+        Toast.makeText(this,"Send Success   Updating",Toast.LENGTH_SHORT).show();
+        Update();
     }
 
     public void fanswitch (View view)
@@ -297,6 +242,8 @@ public class MainActivity extends AppCompatActivity {
             sendrequest1.send(this,"fanswitch",0);}
         else {
             sendrequest1.send(this,"fanswitch",1);}
+        Toast.makeText(this,"Send Success   Updating",Toast.LENGTH_SHORT).show();
+        Update();
 
     }
 
@@ -307,9 +254,12 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "sendrequest1: " +AutoLightswtichtext.getText());
         if (AutoLightswtichtext.getText()=="on")
         {
-            sendrequest1.send(this,"AutoLight",1);}
-        else {
             sendrequest1.send(this,"AutoLight",0);}
+        else {
+            sendrequest1.send(this,"AutoLight",1);}
+
+        Toast.makeText(this,"Send Success   Updating",Toast.LENGTH_SHORT).show();
+        Update();
     }
 
 
@@ -319,11 +269,12 @@ public class MainActivity extends AppCompatActivity {
         sendrequest sendrequest1= new  sendrequest();
         Log.d(TAG, "sendrequest2: " +Autofanswtichtext.getText());
         if (Autofanswtichtext.getText()=="on")
-        {sendrequest1.send(this,"Autofan",1);}
+        {sendrequest1.send(this,"Autofan",0);}
         else {
-            sendrequest1.send(this,"Autofan",0);}
+            sendrequest1.send(this,"Autofan",1);}
 
-
+        Toast.makeText(this,"Send Success   Updating",Toast.LENGTH_SHORT).show();
+        Update();
 
     }
 
