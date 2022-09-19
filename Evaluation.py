65aac517f7176f074a084c0a460942f97a3e7e23
lxm008 <lxm008@student.bham.ac.uk>
2022-09-19 16:18:07 +0100

    to github

--- Evaluation.py
+++ Evaluation.py
@@ -8,10 +8,10 @@ import pandas as pd
 
 TOKEN = "BBFF-VFK6z72XVCSg4ioLSyqbPz3u67UzKk"  # Put your TOKEN here
 DEVICE = "RaspberryPi" # Assign the device label to obtain the variable
-autofanId="autofan"
-autolightId="autolight"
-lightswitchId = "lightswitch"
-fanswitchId= "fanswitch"
+lightId="light"
+humidityId="humidity"
+motionId = "motion"
+temperatureId= "temperature"
 
 s=0 
 err=0
@@ -19,7 +19,7 @@ err=0
 
 def test_var(device, variable):
     delay1=[]
-    for i in range(100):
+    for i in range(500):
         global s
         global err
         try:
@@ -41,13 +41,13 @@ def test_var(device, variable):
         
 
 
-delay2= test_var(DEVICE,autofanId)
-delay3= test_var(DEVICE,autolightId)
-delay4= test_var(DEVICE,lightswitchId)
-delay5= test_var(DEVICE,fanswitchId)
+delay2= test_var(DEVICE,lightId)
+delay3= test_var(DEVICE,humidityId)
+delay4= test_var(DEVICE,motionId)
+delay5= test_var(DEVICE,temperatureId)
 delay2.append(sum(delay2)/len(delay2))
 delay3.append(sum(delay3)/len(delay3))
 delay4.append(sum(delay4)/len(delay4))
 delay5.append(sum(delay5)/len(delay5))
-data = pd.DataFrame({autofanId:delay2,autolightId:delay3,lightswitchId:delay4,fanswitchId:delay5})
-data.to_excel("evaluation.xlsx",sheet_name='sheet1',index=False)
+data = pd.DataFrame({lightId:delay2,humidityId:delay3,motionId:delay4,temperatureId:delay5})
+data.to_excel("androidEvaluation500.xlsx",sheet_name='sheet1',index=False)
